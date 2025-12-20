8af9f36fbe5be6e9314cbcfec3ae1a48860fa619
HOSEKI7 <faridzahran174@gmail.com>
2025-12-20 15:35:11 +0700

    change folder name from 'frontend' to 'frontend-store'

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,10 @@ app.use(cors());
 app.use(express.json());
 app.use(ProductRoute);
 
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
 app.listen(process.env.APP_PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.APP_PORT}`);
 });
