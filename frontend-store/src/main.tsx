442a59a30fc66eba2b241ca53510160f1a7d4deb
HOSEKI7 <faridzahran174@gmail.com>
2025-12-21 23:58:04 +0700

    rename darkMode_tmp.tsx -> darkMode.tsx

--- frontend-store/src/main.tsx
+++ frontend-store/src/main.tsx
@@ -6,7 +6,7 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store/store";
 import "./index.css";
-import DarkModeContextProvider from "./context/darkMode_tmp";
+import DarkModeContextProvider from "./context/darkMode";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
